4590bcb385d3630d5f679bf2c5c224c67119eaf7
Vinezin-hackerwer <viniapelaun306@gmail.com>
2026-03-28 12:49:00 -0300

    Arquivo Aula 1
    Tipos de dados primitivos em Python

---
+++ aula1/Tipos_dados_variaveis.py
@@ -0,0 +1,70 @@
+# indentação -  organizar o código
+
+
+print()
+
+
+
+# variáveis
+
+
+imc = 0
+
+
+# Não podem por numero
+# caracteres especiais # @% &*
+
+
+# pode começar por letra
+# _ underline pode utilizar
+
+
+# seguida de um igual 
+
+
+# ESTRUTURA DE DADO
+
+
+imc = 0 
+altura  =  1.70
+peso = 62.0
+imc =  peso // (altura ** 2)
+print(imc)
+
+
+# sinais aritméticos 
+# +   -    *   /   %   **  // 
+
+
+print('SINAIS ARITMÉTICOS')
+# saida sem é um número 
+
+
+x = 10
+y = 20
+x = 200
+
+
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+print(x ** y)
+
+
+
+print(x // y)
+print(x % y)
+
+
+
+# sinais lógicos 
+y = 20
+x = 200
+print(x == y) # comparação
+print(x > y) # maior
+print(x < y) # menor
+print(x >= y) # maior ou igual
+print(x <= y) # menor ou igual
+print(x != y) # diferente
+
