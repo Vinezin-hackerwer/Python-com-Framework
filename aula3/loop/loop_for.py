e358e66e31c3d77c42628135ec6aa7b048ae095e
Vinezin-hackerwer <viniapelaun306@gmail.com>
2026-04-18 17:46:46 -0300

    Add files via upload

---
+++ aula3/loop/loop_for.py
@@ -0,0 +1,21 @@
+
+
+# notas =  []
+# media = 0
+
+# # finito
+# for i in range(2):
+#     nome =  input('nome:')
+#     nota1 = float(input('nota 1: '))
+#     nota2 = float(input('nota 2: '))  
+#     notas.append(nota1)
+#     notas.append(nota2)
+#     media =  sum(notas) /  len(notas)
+#     print(media)    
+    
+
+
+c =  0
+while c <= 5:
+    print(c)
+    c =  c + 1
